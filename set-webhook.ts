e316596e3633d17973dca8ab6d6448be4f23fb93
Roz <roz@rjmunhoz.me>
2022-08-04 11:15:58 -0300

    chore: add some information to set-webhook script

--- set-webhook.ts
+++ set-webhook.ts
@@ -2,4 +2,6 @@ import { Bot } from 'grammy';
 import { config } from './src/utils/config.ts';
 
 const bot = new Bot(config.telegramToken);
+await bot.init();
+console.log(`Setting webhook to ${Deno.args[0]} for bot ${bot.botInfo.username}`);
 await bot.api.setWebhook(Deno.args[0], { secret_token: config.telegramSecret });
