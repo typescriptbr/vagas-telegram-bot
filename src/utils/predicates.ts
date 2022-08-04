aad3a246d692c96e8ff0bbcf29396be7baf991b4
Roz <roz@rjmunhoz.me>
2022-08-04 11:17:00 -0300

    fix: isAdmin properly accounts for owner status

--- src/utils/predicates.ts
+++ src/utils/predicates.ts
@@ -2,7 +2,7 @@ import { AppContext } from '../bot.ts';
 
 export const isAdmin = (admins: Set<number>) => (ctx: AppContext) =>
   admins.has(ctx.message?.from?.id || 0) ||
-  ['administrator', 'owner'].includes(ctx.chatMember?.new_chat_member?.status || '');
+  ['administrator', 'creator'].includes(ctx.chatMember?.new_chat_member?.status || '');
 
 export const isntAdmin = (admins: Set<number>) => (ctx: AppContext) => !(isAdmin(admins))(ctx);
 export const isGroup = (ctx: AppContext) => ctx.chat?.type === 'group' || ctx.chat?.type === 'supergroup';
